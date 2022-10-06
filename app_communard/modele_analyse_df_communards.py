60985f75695e4f0b9e7844c2adcfc4e2db995254
silanoc <silanoc@gmail.com>
2022-10-06 23:03:02 +0200

    tdd vert, réduction de colonne ok

--- app_communard/modele_analyse_df_communards.py
+++ app_communard/modele_analyse_df_communards.py
@@ -2,8 +2,21 @@
 # coding: utf-8
 
 import logging
+import pandas as pd
 
 logging.basicConfig(level=logging.DEBUG,
                     filename="./app_communard/logs/app.log",
                     filemode="w",
                     format='%(asctime)s - %(levelname)s - %(message)s')
+
+class Analyse_df():
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df: pd.DataFrame = df
+        
+    def recuperer_colonnes_voulues(self, df: pd.DataFrame, *args):
+        """faire un data frame avec les colonnes voulues"""
+        colonnes_voules: list = []
+        for arg in args:
+            colonnes_voules.append(arg)
+        df_reduit: pd.DataFrame = df[colonnes_voules]
+        return df_reduit
--- app_communard/test/test_modele_analyse_df_communards.py
+++ app_communard/test/test_modele_analyse_df_communards.py
@@ -8,14 +8,17 @@ from app_communard.modele_analyse_df_communards import *
 class Test_analyse_df_communards():
     @pytest.fixture()
     def df_de_test(self):
+        """un df pour faire les tests"""
         data = {'communardLabel.value':['Louise Michel', 'Eugène Varlin', 'Valles'],
                     'occupation.value':['communard', 'communard', 'communard'],
                     'communard.type':['uri', 'uri', 'uri'],
-                    'sexe_ou_genreLabel':['féminin', 'masculin', 'Na']
-                    }
+                    'sexe_ou_genreLabel':['féminin', 'masculin', 'Na']}
         df = pd.DataFrame(data)
         return df
     
-    def test_recuperer_colonnes_voulues(self):
-        df_test_reduit: pd.DataFrame = recuper_colonnes_voulues(df_de_test,'occupation')
-        assert df_test_reduit.shape == (3,1)
+    def test_recuperer_colonnes_voulues(self, df_de_test):
+        analyse = Analyse_df(df_de_test)
+        df_test_reduit: pd.DataFrame = analyse.recuperer_colonnes_voulues(df_de_test, 'communardLabel.value','occupation.value')
+        assert df_test_reduit.shape == (3, 2)
+        df_test_reduit: pd.DataFrame = analyse.recuperer_colonnes_voulues(df_de_test, 'communardLabel.value')
+        assert df_test_reduit.shape == (3, 1)
